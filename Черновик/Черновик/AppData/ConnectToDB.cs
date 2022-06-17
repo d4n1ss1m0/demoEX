b9b99875de3d67ab2be61f3a3ba52b17c231dc61
Daniil <dan2002s@yandex.ru>
2022-06-17 23:47:29 +0300

    ver1

---
+++ Черновик/Черновик/AppData/ConnectToDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Черновик.AppData
+{
+    public  class ConnectToDB
+    {
+
+        private static DemoExEntities s_demoEx;
+
+        private  ConnectToDB() { }
+
+        public static DemoExEntities DemoEx { get
+            {
+                if (s_demoEx == null)
+                    s_demoEx = new DemoExEntities();
+                    return s_demoEx;
+            }
+                 }
+    }
+}
---
+++ Черновик/Черновик/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Черновик.Pages;
+using Черновик.ViewModels;
+
+namespace Черновик
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var vm = new MaterialsViewModel();
+            var mainPage = new MaterialPage();
+            mainPage.DataContext = vm;
+            MainFrame.Navigate(mainPage);
+        }
+
+      
+    }
+}
